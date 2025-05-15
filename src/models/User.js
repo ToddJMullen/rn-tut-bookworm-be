641e9053f4a75e3df2bd4ef3b15bff36f99718dc
= <=>
2025-05-15 18:34:32 -0400

    Task: Add userSchema log detail

--- src/models/User.js
+++ src/models/User.js
@@ -34,6 +34,7 @@ userSchema.pre("save", async function (next) {
 
   const salt = await bcrypt.getSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  console.log("userSchema.pre/", {salt, hash: this.password});
   next();
 });
 
