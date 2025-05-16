a8079fe681655ccadecea05c5ecd75628908b946
= <=>
2025-05-16 15:58:03 -0400

    Fix: add cloudinary log check

--- src/lib/cloudinary.js
+++ src/lib/cloudinary.js
@@ -1,6 +1,8 @@
 import {v2 as cloudinary} from "cloudinary";
 import "dotenv/config";
 
+console.log("cloudinary/ end", process.env.CLOUDINARY_API_KEY.slice(-5)); 
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
